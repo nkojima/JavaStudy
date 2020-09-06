1a9983dfb3694bcd33d4608c5616da1b7a97642b
kojiro629 <kojiro629@gmail.com>
2020-09-06 23:46:49 +0900

    Add 'ZunDokoStream.java'.

---
+++ src/algorithm/zundoko/ZundokoStream.java
@@ -0,0 +1,28 @@
+package algorithm.zundoko;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ZundokoStream {
+
+	public static void main(String[] args) {
+        Random rnd = new Random();
+        String[] zunDoko = { "ズン", "ドコ" };
+        String songLyrics = "";
+
+        do {
+            List<String> phrases =
+                    Stream.generate(() -> zunDoko[rnd.nextInt(2)])
+                    .limit(5)
+                    .collect(Collectors.toList());
+
+            songLyrics = String.join(" ", phrases);
+        } while (!songLyrics.equals("ズン ズン ズン ズン ドコ"));
+
+        System.out.print(songLyrics);
+        System.out.println(" キヨシ！！");
+	}
+
+}
