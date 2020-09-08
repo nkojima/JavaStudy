70f1c627be6c864fcd3cc056f1252f4d0753916f
kojiro629 <kojiro629@gmail.com>
2020-09-08 22:44:02 +0900

    Add 'Concatenation.java'.

---
+++ src/string/concat/Concatenation.java
@@ -0,0 +1,65 @@
+package string.concat;
+
+public class Concatenation {
+
+	public static void main(String[] args) {
+        plus();
+        concat();
+        builder();
+        buffer();
+	}
+
+    public static void plus() {
+
+        long start = System.currentTimeMillis();
+        String txt = "abc";
+
+        for (int i=0; i<100000; i++) {
+            txt += "abc";
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println("+:" + (end - start) + "ms");
+    }
+
+    public static void concat() {
+
+        long start = System.currentTimeMillis();
+        String txt = "abc";
+
+        for (int i=0; i<100000; i++) {
+            txt = txt.concat("abc");
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println("concat:" + (end - start) + "ms");
+    }
+
+    public static void builder() {
+
+        long start = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder("abc");
+
+        for (int i=0; i<100000; i++) {
+            sb.append("abc");
+        }
+
+        String txt = sb.toString();
+        long end = System.currentTimeMillis();
+        System.out.println("StringBuilder:" + (end - start) + "ms");
+    }
+
+    public static void buffer() {
+
+        long start = System.currentTimeMillis();
+        StringBuffer sb = new StringBuffer("abc");
+
+        for (int i=0; i<100000; i++) {
+            sb.append("abc");
+        }
+
+        String txt = sb.toString();
+        long end = System.currentTimeMillis();
+        System.out.println("StringBuffer:" + (end - start) + "ms");
+    }
+}
