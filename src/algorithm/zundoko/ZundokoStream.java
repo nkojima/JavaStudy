a2747781963bb35939240b373da8329aaa0d2372
kojiro629 <kojiro629@gmail.com>
2020-09-08 22:48:17 +0900

    Improve comments of 'ZundokoStream.java'.

--- src/algorithm/zundoko/ZundokoStream.java
+++ src/algorithm/zundoko/ZundokoStream.java
@@ -17,6 +17,13 @@ public class ZundokoStream {
         String songLyrics = "";
 
         do {
+        	/*
+        	 * 「ズン」もしくは「ドコ」のいずれかを格納した長さ5のListを作ります。
+        	 * Stream.generate()を使う場合、Streamが無限に続かないようにlimitを付けるのがポイントです。
+        	 *
+        	 * Listを半角スペースでJoinした結果が「ズン ズン ズン ズン ドコ」になるまで、do～whileのループを繰り返します。
+        	 * 必ず1回はループに入るので、最初に終了条件を判定するwhileではなく、最後に終了条件を判定するdo～whileを使いました。
+        	 */
             List<String> phrases =
                     Stream.generate(() -> zunDoko[rnd.nextInt(2)])
                     .limit(5)
@@ -25,6 +32,7 @@ public class ZundokoStream {
             songLyrics = String.join(" ", phrases);
         } while (!songLyrics.equals("ズン ズン ズン ズン ドコ"));
 
+        // 最後に「キヨシ！！」を付け足して標準出力します。
         System.out.print(songLyrics);
         System.out.println(" キヨシ！！");
 	}
