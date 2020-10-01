c8e76f91f421c1b2a4d184188944290a7d4f4c4e
kojiro629 <kojiro629@gmail.com>
2020-10-01 21:43:55 +0900

    Add 'sortDescending()'. #17

--- src/algorithm/sort/BubbleSort.java
+++ src/algorithm/sort/BubbleSort.java
@@ -14,6 +14,7 @@ public class BubbleSort {
 
 	public static void main(String[] args) {
 		sortAscending();
+		sortDescending();
 	}
 
 	/**
@@ -31,6 +32,31 @@ public class BubbleSort {
 		for (int i=0; i<length-1; i++) {
 			for (int j=length-1; i<j; j--) {
 				if (list.get(j)<list.get(j-1)) {
+					tmp = list.get(j);
+					list.set(j, list.get(j-1));
+					list.set(j-1, tmp);
+				}
+			}
+		}
+
+		System.out.println("処理後の状態：" + list.toString());
+	}
+
+	/**
+	 * 降順に並べ替える。
+	 */
+	private static void sortDescending() {
+		System.out.println("降順に並べ替える");
+
+		List<Integer> list = createIntList();
+		System.out.println("初期状態：" + list.toString());
+
+		int length = list.size();
+		int tmp;
+
+		for (int i=0; i<length-1; i++) {
+			for (int j=length-1; i<j; j--) {
+				if (list.get(j)>list.get(j-1)) {
 					tmp = list.get(j);
 					list.set(j, list.get(j-1));
 					list.set(j-1, tmp);
