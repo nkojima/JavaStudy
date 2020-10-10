fbf6cd8c0f4b4e95301d6227752bc8be54298ca5
kojiro629 <kojiro629@gmail.com>
2020-10-10 23:36:40 +0900

    Add superclass of BubbleSort class to algorithm.sort package. #20

--- src/algorithm/sort/BubbleSort.java
+++ src/algorithm/sort/BubbleSort.java
@@ -1,8 +1,5 @@
 package algorithm.sort;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -10,7 +7,7 @@ import java.util.List;
  * @author NKOJIMA
  *
  */
-public class BubbleSort {
+public class BubbleSort extends Sort {
 
 	public static void main(String[] args) {
 		sortAscending();
@@ -65,19 +62,5 @@ public class BubbleSort {
 		}
 
 		System.out.println("処理後の状態：" + list.toString());
-	}
-
-
-	/**
-	 * 1～10までの値がランダムに並んだリストを返す。
-	 * @return
-	 */
-	private static List<Integer> createIntList() {
-		// 1～10のリストを作り、並び順をシャッフルする。
-		List<Integer> list =
-				new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-		Collections.shuffle(list);
-
-		return list;
 	}
 }
