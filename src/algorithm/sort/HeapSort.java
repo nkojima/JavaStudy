012e39094881a807cce4bb2b8f3972c17a764aa6
kojiro629 <kojiro629@gmail.com>
2020-10-25 23:57:28 +0900

    Add comment. #21

--- src/algorithm/sort/HeapSort.java
+++ src/algorithm/sort/HeapSort.java
@@ -8,4 +8,12 @@ package algorithm.sort;
 public class HeapSort {
 
 	// http://www.th.cs.meiji.ac.jp/assets/researches/2005/omoto/heapsort.html
+
+	/**
+	 * ■ヒープの実装方法（インデックスが0から始まる場合）
+	 * ノードnに対して...
+	 * 親：(n-1) / 2
+	 * 左の子：2 * n + 1
+	 * 右の子：2 * n + 2
+	 */
 }
