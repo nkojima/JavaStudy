a66758772c7180d92d5faa767f5f557bf5e64868
kojiro629 <kojiro629@gmail.com>
2020-11-16 22:52:46 +0900

    Add sample code of java.time.format.DateTimeFormatter. #10

---
+++ src/java8/localdate/DateTextParser.java
@@ -0,0 +1,25 @@
+package java8.localdate;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateTextParser {
+
+	private static final String DATE_STRING = "2020-11-07";
+
+	private static final String INVALID_DATE_STRING = "2019-02-29";
+
+	public static void main(String[] args) {
+		LocalDate dt = LocalDate.parse(DATE_STRING, DateTimeFormatter.ISO_DATE);
+		System.out.println("日付は" + dt + "です。");
+
+		// 不正な日付をパースすると、DateTimeExceptionが送出される。
+		try {
+			LocalDate.parse(INVALID_DATE_STRING, DateTimeFormatter.ISO_DATE);
+		} catch (DateTimeException dte) {
+			dte.printStackTrace();
+		}
+	}
+
+}
