750aef9d2979b6dc2d713d1afb128b9f2db38a26
kojiro629 <kojiro629@gmail.com>
2020-11-28 22:41:21 +0900

    Add exception pattern to DateTextParser.java. #10

--- src/java8/localdate/DateTextParser.java
+++ src/java8/localdate/DateTextParser.java
@@ -7,7 +7,7 @@ import java.time.format.DateTimeFormatter;
 /**
  * 日付文字列をLocalDate型に変換するサンプルコード。
  * @author NKOJIMA
- * 
+ *
  */
 public class DateTextParser {
 
@@ -15,13 +15,22 @@ public class DateTextParser {
 
 	private static final String INVALID_DATE_STRING = "2019-02-29";
 
+	private static final String INVALID_FORMAT_DATE_STRING = "2019/02/28";
+
 	public static void main(String[] args) {
 		LocalDate dt = LocalDate.parse(DATE_STRING, DateTimeFormatter.ISO_DATE);
 		System.out.println("日付は" + dt + "です。");
 
-		// 不正な日付をパースすると、DateTimeExceptionが送出される。
+		// 存在しない不正な日付をパースしようとすると、DateTimeExceptionが送出される。
 		try {
 			LocalDate.parse(INVALID_DATE_STRING, DateTimeFormatter.ISO_DATE);
+		} catch (DateTimeException dte) {
+			dte.printStackTrace();
+		}
+
+		// フォーマットに適合しない不正な日付をパースしようとすると、DateTimeExceptionが送出される。
+		try {
+			LocalDate.parse(INVALID_FORMAT_DATE_STRING, DateTimeFormatter.ISO_DATE);
 		} catch (DateTimeException dte) {
 			dte.printStackTrace();
 		}
