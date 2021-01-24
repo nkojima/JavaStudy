55e68dbfa88383ad6514cba5117fe5d199505c78
kojiro629 <kojiro629@gmail.com>
2021-01-24 21:26:30 +0900

    Add comment to 'GenericList.java'.

--- src/generics/GenericList.java
+++ src/generics/GenericList.java
@@ -8,11 +8,19 @@ public class GenericList {
 	public static void main(String[] args) {
 		System.out.println(getList(true).get(0).getClass());	// String型のListが返される。
 		System.out.println(getList(false).get(0).getClass());	// Integer型のListが返される。
+
+		/*
+		 * 非境界ワイルドカードのListは実行されるまでデータ型が分からないため、
+		 * addすることができない。
+		 */
+		//List<?> list = getList(true);
+		//list.add(123);
 	}
 
 	/**
 	 * 引数がtrueであればStringのList、falseであればIntegerのListを返す。
 	 * ※実行されるまで戻り値のListの要素の型が不明なので、ワイルドカードを使用している。
+	 * ※「?」は非境界ワイルドカードと呼ばれる。
 	 * @param flag
 	 * @return
 	 */
