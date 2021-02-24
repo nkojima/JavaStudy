c0892a85c52d562b81f81d61e8a3dfbfd136c84e
kojiro629 <kojiro629@gmail.com>
2021-02-24 22:43:48 +0900

    Add sample code of method reference.

---
+++ src/java8/method_reference/StaticMethodReference.java
@@ -0,0 +1,15 @@
+package java8.method_reference;
+
+/**
+ * staticメソッド参照のサンプルコード。
+ * @author NKOJIMA
+ *
+ */
+public class StaticMethodReference {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
