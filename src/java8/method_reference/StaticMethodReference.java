dbd32f35fe57fa043606f71dc5a3f7bf256229a5
kojiro629 <kojiro629@gmail.com>
2021-02-24 23:07:13 +0900

    no message

--- src/java8/method_reference/StaticMethodReference.java
+++ src/java8/method_reference/StaticMethodReference.java
@@ -1,5 +1,10 @@
 package java8.method_reference;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+
 /**
  * staticメソッド参照のサンプルコード。
  * @author NKOJIMA
@@ -8,8 +13,24 @@ package java8.method_reference;
 public class StaticMethodReference {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		List<Integer> list = Arrays.asList(3, 1, 2);
 
+		// 匿名クラスを使用した場合。
+		Consumer<List<Integer>> con1 = new Consumer<List<Integer>>() {
+			public void accept(List<Integer> list) {
+				Collections.sort(list);
+			}
+		};
+		con1.accept(list);
+
+		// ラムダ式を使用した場合。
+		Consumer<List<Integer>> con2 = lambdaList -> Collections.sort(list);
+		con2.accept(list);
+
+		// メソッド参照を使用した場合。
+		Consumer<List<Integer>> con3 = Collections::sort;
+		con3.accept(list);
+
+		System.out.println(list);
 	}
-
 }
