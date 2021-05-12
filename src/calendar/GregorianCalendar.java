f38b4619a18a6880f8c853d18e1c29c912730179
kojiro629 <kojiro629@gmail.com>
2021-05-12 22:27:59 +0900

    Add comment and fix variable name.

--- src/calendar/GregorianCalendar.java
+++ src/calendar/GregorianCalendar.java
@@ -5,9 +5,9 @@ import java.time.LocalDate;
 
 public class GregorianCalendar {
 	public static void main(String[] args) {
-		for (int i=1990; i<=2020; i++) {
-			System.out.println(i + "年：" + isLeapYear(i));
-			System.out.println(i + "年：" + isLeapYear2(i));
+		for (int year=1990; year<=2020; year++) {
+			System.out.println(year + "年：" + isLeapYear(year));
+			System.out.println(year + "年：" + isLeapYear2(year));
 		}
 
 	}
@@ -15,7 +15,7 @@ public class GregorianCalendar {
 	/**
 	 * 引数の西暦年が「うるう年」であるかを判定する。
 	 * @param year
-	 * @return
+	 * @return うるう年であればtrue、それ以外の年はfalseを返す。
 	 */
 	static boolean isLeapYear(int year) {
 		if (year%4==0 && year%100!=0) {
@@ -30,7 +30,7 @@ public class GregorianCalendar {
 	/**
 	 * LocalDateクラスの例外を利用して、引数の西暦年が「うるう年」であるかを判定する。
 	 * @param year
-	 * @return
+	 * @return うるう年であればtrue、それ以外の年はfalseを返す。
 	 */
 	static boolean isLeapYear2(int year) {
 
