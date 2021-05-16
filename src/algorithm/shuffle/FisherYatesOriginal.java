2f01bf10cd5f92f9a3e84ccdfd80f08346d74c93
kojiro629 <kojiro629@gmail.com>
2021-05-16 22:38:55 +0900

    Add 2 class files of Fisher-Yates shuffle.

---
+++ src/algorithm/shuffle/FisherYatesOriginal.java
@@ -0,0 +1,14 @@
+package algorithm.shuffle;
+
+/**
+ * フィッシャー・イェーツのシャッフル（1938年のオリジナル版）
+ * @author NKOJIMA
+ *
+ */
+public class FisherYatesOriginal {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/algorithm/shuffle/FisherYatesRefined.java
@@ -0,0 +1,14 @@
+package algorithm.shuffle;
+
+/**
+ * フィッシャー・イェーツのシャッフルの改良版
+ * @author NKOJIMA
+ *
+ */
+public class FisherYatesRefined {
+
+	public static void main(String[] args) {
+
+	}
+
+}
